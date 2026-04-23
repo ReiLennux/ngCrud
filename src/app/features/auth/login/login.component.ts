4c786a4396ddbf6e5c928de032b83f3944b8d39e
Lenn <lennyrm343@outlook.com>
2026-04-22 19:39:08 -0600

    refactor: login ui

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -3,27 +3,27 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css'],
-    standalone: false
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: false
 })
 export class LoginComponent {
   userData = {
     email: '',
-    password: ''
+    strPassword: ''
   };
 
   loading: boolean = false;
 
-  constructor(private router: Router, private auth: AuthService) {}
+  constructor(private router: Router, private auth: AuthService) { }
 
   onSubmit(): void {
     this.loading = true;
 
     this.auth.login(this.userData).subscribe({
       next: res => {
-        this.userData = {email: '', password: ''};
+        this.userData = { email: '', strPassword: '' };
         this.loading = false;
         this.router.navigate(['/home']);
       },
